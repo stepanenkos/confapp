5445ce4306aefa993e159d0142dc3225740c83b1
Vladimir Tokarev <vladimir@Vladimirs-MacBook-Pro.local>
2020-10-19 19:20:20 +0600

    First commit

---
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package kz.kolesateam.confapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kz.kolesateam.confapp.hello.presentation.HelloActivity
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    private val openHelloButton: Button by lazy {
+        findViewById(R.id.activity_main_open_hello_button)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        openHelloButton.setOnClickListener {
+            val helloScreenIntent = Intent(this, HelloActivity::class.java)
+            startActivity(helloScreenIntent)
+        }
+
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
@@ -0,0 +1,63 @@
+package kz.kolesateam.confapp.hello.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kz.kolesateam.confapp.R
+
+private const val TAG = "HelloActivity"
+
+class HelloActivity : AppCompatActivity() {
+
+    private val closeHelloButton: Button by lazy {
+        findViewById(R.id.activity_hello_close_hello_button)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+
+        closeHelloButton.setOnClickListener {
+            finish()
+        }
+
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+
+        super.onDestroy()
+    }
+}
