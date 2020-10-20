d74025198997a5744d1ac807319a664a16f8b570
stepanenkos <s_sv3006@mail.ru>
2020-10-20 22:14:03 +0600

    Added AboutActivity.kt, changed MainActivity.kt

--- app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import android.widget.Button
 import kz.kolesateam.confapp.hello.presentation.HelloActivity
 
-private const val TAG = "MainActivity"
+private const val TAG = "MainActivityTAG"
 
 class MainActivity : AppCompatActivity() {
 
