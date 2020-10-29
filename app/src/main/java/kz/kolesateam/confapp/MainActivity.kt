81abc1de20a73c4e8c7e346f543b96eb80eb63a7
Sergey Stepanenko <ssv2408@gmail.com>
2020-10-30 01:17:37 +0600

    Removed all lifecycle methods except onCreate

--- app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/MainActivity.kt
@@ -23,43 +23,5 @@ class MainActivity : AppCompatActivity() {
             val helloScreenIntent = Intent(this, HelloActivity::class.java)
             startActivity(helloScreenIntent)
         }
-
-        Log.d(TAG, "onCreate")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-
-        Log.d(TAG, "onRestart")
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        Log.d(TAG, "onPause")
-
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG, "onStop")
-
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "onDestroy")
-
-        super.onDestroy()
     }
 }
