485ddbad74c8dae26daf4706b1c8dce60cc033d4
Sergey Stepanenko <ssv2408@gmail.com>
2020-11-04 14:13:37 +0600

    Переименована переменная для TextView

--- app/src/main/java/kz/kolesateam/confapp/hello/presentation/TestHelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/hello/presentation/TestHelloActivity.kt
@@ -10,9 +10,9 @@ class TestHelloActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_hello)
-        val textView: TextView = findViewById(R.id.textViewForName)
+        val helloTextView: TextView = findViewById(R.id.textViewForName)
         val sharedPref = getSharedPreferences(SHARED_PREFERENCES_NAME_KEY, Context.MODE_PRIVATE)
         val name = sharedPref.getString(SHARED_PREFERENCES_NAME_KEY, "Пользователь")
-        textView.text = name
+        helloTextView.text = name
     }
 }
