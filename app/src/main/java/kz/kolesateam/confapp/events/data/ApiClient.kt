170e5de99a138f7b3501acd9ed7f193206b45f37
Sergey Stepanenko <ssv2408@gmail.com>
2020-11-11 23:09:21 +0600

    В интерфейсе ApiClient определил 2 метода

--- app/src/main/java/kz/kolesateam/confapp/events/data/ApiClient.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/data/ApiClient.kt
@@ -1,11 +1,15 @@
 package kz.kolesateam.confapp.events.data
 
 import com.fasterxml.jackson.databind.JsonNode
+import kz.kolesateam.confapp.events.data.models.BranchApiData
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiClient {
 
     @GET("/upcoming_events")
-    fun getUpcomingEvents(): Call<JsonNode>
+    fun getUpcomingEventsSync(): Call<ResponseBody>
+    @GET("/upcoming_events")
+    fun  getUpcomingEventsAsync(): Call<List<BranchApiData>>
 }
