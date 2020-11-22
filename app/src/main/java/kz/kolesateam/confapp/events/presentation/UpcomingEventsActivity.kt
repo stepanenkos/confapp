4f141402a657e95ecb7dae6b3b1b7ad5d322cf77
Sergey Stepanenko <ssv2408@gmail.com>
2020-11-23 01:35:24 +0600

    Добавлен interface Listener для View

--- app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
@@ -1,13 +1,15 @@
 package kz.kolesateam.confapp.events.presentation
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.asLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -16,122 +18,111 @@ import kz.kolesateam.confapp.events.data.ApiClient
 import kz.kolesateam.confapp.events.data.ApiClientSingleton
 import kz.kolesateam.confapp.events.data.JetpackDataStore
 import kz.kolesateam.confapp.events.data.models.BranchApiData
-import kz.kolesateam.confapp.events.data.models.FormattingString
+import kz.kolesateam.confapp.events.data.models.UpcomingEventsListItem
 import kz.kolesateam.confapp.events.data.models.UpcomingEventsRepository
-import retrofit2.converter.jackson.JacksonConverterFactory
+import kz.kolesateam.confapp.events.presentation.view.EventClickListener
+import kz.kolesateam.confapp.events.presentation.view.UpcomingEventsAdapter
 import kz.kolesateam.confapp.extensions.gone
 import kz.kolesateam.confapp.extensions.show
+import kz.kolesateam.confapp.hello.presentation.SHARED_PREFERENCES_NAME_KEY
+import kz.kolesateam.confapp.hello.presentation.USER_NAME
+import retrofit2.converter.jackson.JacksonConverterFactory
 
-class UpcomingEventsActivity : AppCompatActivity() {
+private const val DEFAULT_USER_NAME = "Гость"
 
-    private lateinit var syncLoadingButton: Button
-    private lateinit var asyncLoadingButton: Button
-    private lateinit var upcomingEventsTextView: TextView
+class UpcomingEventsActivity : AppCompatActivity(), EventClickListener {
+
     private lateinit var upcomingEventsProgressBar: ProgressBar
+
+    private lateinit var recyclerView: RecyclerView
+    private val adapter = UpcomingEventsAdapter(this)
+
+    private val upcomingEventsRepository = UpcomingEventsRepository()
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var userName: String
+    private var isPressedToFavoritesButton = true
     private val apiClient: ApiClient = ApiClientSingleton.getApiClient(
         "http://37.143.8.68:2020",
         JacksonConverterFactory.create(),
         ApiClient::class.java
     )
-    private lateinit var jetpackDataStore: JetpackDataStore
-    private lateinit var branchApiData: List<BranchApiData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upcoming_events)
         initViews()
-
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        saveData()
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        restoreData()
-        super.onRestoreInstanceState(savedInstanceState)
+        getUpcomingEvents()
     }
 
     private fun initViews() {
-        syncLoadingButton = findViewById(R.id.activity_upcoming_events_btn_sync_loading)
-        asyncLoadingButton = findViewById(R.id.activity_upcoming_events_btn_async_loading)
-        upcomingEventsTextView = findViewById(R.id.activity_upcoming_events_text_view)
         upcomingEventsProgressBar = findViewById(R.id.activity_upcoming_events_progress_bar)
 
-        jetpackDataStore = JetpackDataStore(this)
-
-        syncLoadingButton.setOnClickListener {
-            upcomingEventsProgressBar.show()
-            loadSyncData()
-        }
-
-        asyncLoadingButton.setOnClickListener {
-            upcomingEventsProgressBar.show()
-            loadAsyncData()
-        }
-    }
-
-private fun loadSyncData() {
-    GlobalScope.launch(Dispatchers.IO) {
-        runCatching {
-            branchApiData = UpcomingEventsRepository().loadUpcomingEventsSync()
-            launch(Dispatchers.Main) {
-                setViewAttributeOnResponse(
-                    branchApiData,
-                    R.color.activity_upcoming_events_sync_text_view
-                )
-            }
-        }.onFailure { t ->
-            GlobalScope.launch(Dispatchers.Main) {
-                setViewAttributeOnFailure(t.localizedMessage!!)
-            }
-        }
-    }
-}
-    private fun loadAsyncData() {
-        UpcomingEventsRepository().getUpcomingEventsAsync({
-            branchApiData = it
-            setViewAttributeOnResponse(branchApiData, R.color.activity_upcoming_events_async_text_view)
-        },
-            {
-                setViewAttributeOnFailure(it)
-            })
-
-    }
-
-    private fun setViewAttributeOnResponse(list: List<BranchApiData>, textColor: Int) {
-            upcomingEventsProgressBar.gone()
-            upcomingEventsTextView.setTextColor(
-                ContextCompat.getColor(this, textColor)
-            )
-            upcomingEventsTextView.text = FormattingString.formatString(list)
-    }
-
-    private fun setViewAttributeOnFailure(throwable: String) {
-        upcomingEventsProgressBar.gone()
-        upcomingEventsTextView.setTextColor(
-            ContextCompat.getColor(this, R.color.activity_upcoming_events_error_text_view)
+        recyclerView = findViewById(R.id.activity_upcoming_events_recycler_view)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(
+            this
         )
-        upcomingEventsTextView.text = throwable
+
+        sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME_KEY, Context.MODE_PRIVATE)
+        userName = sharedPreferences.getString(SHARED_PREFERENCES_NAME_KEY, DEFAULT_USER_NAME)!!
     }
 
-    private fun saveData() {
-        GlobalScope.launch(Dispatchers.IO) {
-            jetpackDataStore.saveData(
-                upcomingEventsTextView.text.toString(),
-                upcomingEventsTextView.currentTextColor
-            )
+    override fun onFavoritesImageClick(view: View) {
+        when (view.id) {
+            R.id.activity_upcoming_events_image_to_favorites -> {
+                view as ImageButton
+                if (isPressedToFavoritesButton) {
+                    view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_to_favorites_fill))
+                } else {
+                    view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_to_favorites_border))
+                }
+                isPressedToFavoritesButton = !isPressedToFavoritesButton
+            }
         }
     }
 
-    private fun restoreData() {
-        jetpackDataStore.downloadedDataFlow.asLiveData(Dispatchers.Main).observe(this, {
-            upcomingEventsTextView.text = it.toString()
-        })
-        jetpackDataStore.colorTextFlow.asLiveData(Dispatchers.Main).observe(this, {
-            upcomingEventsTextView.setTextColor(it)
-
-        })
+    override fun onBranchClick(view: View, branchTitle: String?) {
+        when (view.id) {
+            R.id.activity_upcoming_events_branch_row ->
+                Toast.makeText(this, "Branch: $branchTitle", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    override fun onEventClick(view: View, branchTitle: String?, eventTitle: String?) {
+        when (view.id) {
+            R.id.activity_upcoming_events_current_event ->
+                Toast.makeText(this, "Branch: $branchTitle, Event: $eventTitle", Toast.LENGTH_SHORT)
+                    .show()
+
+            R.id.activity_upcoming_events_next_event ->
+                Toast.makeText(this, "Branch: $branchTitle, Event: $eventTitle", Toast.LENGTH_SHORT)
+                    .show()
+        }
+    }
+
+    private fun getUpcomingEvents() {
+        upcomingEventsProgressBar.show()
+        upcomingEventsRepository.getUpcomingEvents(
+            getString(
+                R.string.activity_upcoming_events_text_view_hello_user,
+                userName
+            ),
+            result = { listBranchApiData ->
+                showResult(listBranchApiData)
+            },
+            fail = { errorMessage ->
+                showError(errorMessage)
+            })
+        upcomingEventsProgressBar.gone()
+    }
+
+    private fun showError(errorMessage: String) {
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showResult(listBranchApiData: List<UpcomingEventsListItem>) {
+        adapter.setList(listBranchApiData)
+    }
+
 }
---
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/view/EventClickListener.kt
@@ -0,0 +1,20 @@
+package kz.kolesateam.confapp.events.presentation.view
+
+import android.view.View
+
+interface EventClickListener {
+    fun onEventClick(
+        view: View,
+        branchTitle: String? = "",
+        eventTitle: String? = ""
+    )
+
+    fun onBranchClick(
+        view: View,
+        branchTitle: String? = "",
+    )
+
+    fun onFavoritesImageClick(
+        view: View
+    )
+}
