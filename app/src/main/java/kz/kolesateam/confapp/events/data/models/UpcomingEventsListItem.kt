d91a8f17b4858f859b548ffd58873da9a1cb885d
Sergey Stepanenko <ssv2408@gmail.com>
2020-12-01 02:47:27 +0600

    Добавлены айтэмы для всех ивентов и названия потока

--- app/src/main/java/kz/kolesateam/confapp/events/data/models/UpcomingEventsListItem.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/data/models/UpcomingEventsListItem.kt
@@ -1,9 +1,12 @@
 package kz.kolesateam.confapp.events.data.models
 
 import kz.kolesateam.confapp.events.presentation.models.BranchData
+import kz.kolesateam.confapp.events.presentation.models.EventData
 
 const val HEADER_TYPE: Int = 0
-const val EVENT_TYPE: Int = 1
+const val BRANCH_TYPE: Int = 1
+const val EVENT_TYPE: Int = 2
+const val BRANCH_TITLE_TYPE: Int = 3
 
 sealed class UpcomingEventsListItem(
     val type: Int,
@@ -14,5 +17,13 @@ sealed class UpcomingEventsListItem(
 
     data class BranchListItem(
         val data: BranchData,
+    ) : UpcomingEventsListItem(BRANCH_TYPE)
+
+    data class EventListItem(
+        val data: EventData,
     ) : UpcomingEventsListItem(EVENT_TYPE)
+
+    data class BranchTitleItem(
+        val branchTitle: String,
+    ) : UpcomingEventsListItem(BRANCH_TITLE_TYPE)
 }
