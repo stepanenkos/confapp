adaa471c08a68c036d88bfce2bc495084cd7c296
Sergey Stepanenko <ssv2408@gmail.com>
2020-12-01 02:25:33 +0600

    Добавлен ApiClient AllEventsDataSource для запросов всех ивентов

---
+++ app/src/main/java/kz/kolesateam/confapp/events/data/datasource/AllEventsDataSource.kt
@@ -0,0 +1,13 @@
+package kz.kolesateam.confapp.events.data.datasource
+
+import kz.kolesateam.confapp.events.data.models.BranchApiData
+import kz.kolesateam.confapp.events.data.models.EventApiData
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AllEventsDataSource {
+
+    @GET("/branch_events/{branch_id}")
+    fun getAllEvents(@Path("branch_id") branchId: Int): Call<List<EventApiData>>
+}
